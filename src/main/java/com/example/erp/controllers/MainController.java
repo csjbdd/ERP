9f149e06f4387a3ae88b21f4e6e84a30ee12f41c
csjbdd <csjbdd2017@gmail.com>
2023-02-22 22:50:16 +0900

    commit

---
+++ src/main/java/com/example/erp/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.example.erp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+    @RequestMapping("/main")
+    public String main() {
+
+        return "index";
+    }
+}
