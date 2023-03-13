3a6498949eaf48f9d333d3c13528e041b013f33f
csjbdd <csjbdd2017@gmail.com>
2023-03-13 22:37:42 +0900

    commit

--- src/main/java/com/example/erp/controllers/MainController.java
+++ src/main/java/com/example/erp/controllers/MainController.java
@@ -1,13 +1,15 @@
 package com.example.erp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
-    @RequestMapping("/main")
-    public String main() {
 
+    @GetMapping("/")
+    public String getIndexPage() {
         return "index";
     }
 }
